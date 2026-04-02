0e2b822315cd3bdf583d131db13d678b439f4165
unknown <gaboliveira.lucas@gmail.com>
2026-04-02 11:19:57 -0300

    gwduqdvuywfe

--- OrderBook.hpp
+++ OrderBook.hpp
@@ -6,12 +6,26 @@ class OrderBook {
 
 private:
 
+struct OrderNode {
+    Order sell_orders;
+    Order* next_sell;
+};
+
+int n_sell;
+int n_buy;
+struct list_transaction {
+
+};
+
+
+
+
+
     // Estruturas internas escolhidas pelos alunos
     // para armazenar ordens de compra, venda e transações
-
 public:
 
-    OrderBook();
+    OrderBook(int n_sell, int n_buy);
     ~OrderBook();
 
     bool submit(Order order);
@@ -20,7 +34,8 @@ public:
     Order* getBuyOrders(int* n);
     Order* getSellOrders(int* n);
     Transaction* getTransactions(int* n);
-
+    int getNumBuyOrders();
+    int getNumSellOrders();
     void printBuyOrders();
     void printSellOrders();
     void printTransactions();
